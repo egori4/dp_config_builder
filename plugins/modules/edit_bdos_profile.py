e1bddb290c8a134e3209597981e3ee82d1ef59aa
Egor Egorov <egore@radware.com>
2025-09-18 17:27:36 -0400

    V0.1.3 fixed bug with edit bdos - wrong action key for block and report

--- plugins/modules/edit_bdos_profile.py
+++ plugins/modules/edit_bdos_profile.py
@@ -167,7 +167,7 @@ def map_netflood_profile_parameters(params):
         "tcp_frag_flood": {"enable": "1", "disable": "2"},
         "udp_frag_flood": {"enable": "1", "disable": "2"},
         "transparent_optimization": {"enable": "1", "disable": "2"},
-        "action": {"report_only": "0", "block_report": "1"},
+        "action": {"report_only": "0", "block_and_report": "1"},
         "burst_attack": {"enable": "1", "disable": "2"},
         "footprint_strictness": {"low": "0", "medium": "1", "high": "2"},
         "bdos_rate_limit": {"disable": "0", "normal_edge": "1", "suspect_edge": "2", "user_defined": "3"},
